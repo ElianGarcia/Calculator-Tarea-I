e5d7ae353fbe4b216cb41381597aa7fc01f853b9
Elian Garcia Rodriguez <elia_garcia@ucne.edu.do>
2020-05-18 09:39:25 -0400

    Add project files.

---
+++ Models/Operaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class Operaciones
+    {
+        [Required(ErrorMessage = "La descripción no puede estar vacía.")]
+        public int primerNumero;
+
+        [Required(ErrorMessage = "La descripción no puede estar vacía.")]
+        public int segundoNumero;
+        public decimal resultado;
+
+        public Operaciones()
+        {
+            primerNumero = 0;
+            segundoNumero = 0;
+            resultado = 0;
+        }
+    }
+}
